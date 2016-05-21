bfad93d3b0079a21b58f8709f3e056b495103dd1
ivan <ivanlsalvadori@gmail.com>
2016-05-21 12:57:21 -0300

    Versão de correção para permitir o donwnload. A versão anterior desativou o download dos boletins.

--- src/main/java/br/com/ivansalvadori/ssp/sp/cleaner/main/Main.java
+++ src/main/java/br/com/ivansalvadori/ssp/sp/cleaner/main/Main.java
@@ -17,37 +17,36 @@ import br.com.ivansalvadori.ssp.sp.cleaner.ListaOcorrenciasPoliciaisExtractor;
 public class Main {
 
 	private static String opcao;
-	
-    @Autowired
-    private Config config;
 
-    @Autowired
-    private ListaOcorrenciasPoliciaisExtractor listaOcorrenciasPoliciaisExtractor;
+	@Autowired
+	private Config config;
 
-    @Autowired
-    private BoletimOcorrenciasExtractor boletimOcorrenciasExtractor;
+	@Autowired
+	private ListaOcorrenciasPoliciaisExtractor listaOcorrenciasPoliciaisExtractor;
 
-    @PostConstruct
-    public void iniciarDownload() throws IOException, InterruptedException {
-    	if(opcao.equalsIgnoreCase("baixar")){
-    	//	listaOcorrenciasPoliciaisExtractor.parseListaEdownloadBOs(config.getPastaPaginasListagem(), config.getPastaLeituraBOs(), config.getSessionId());
-    	}
-    	
-    	else if(opcao.equalsIgnoreCase("processar")){
-    		boletimOcorrenciasExtractor.parseDocument(config.getPastaLeituraBOs(), config.getPastaGravacaoBOsProcessados());
-    	}
-    	
-    	else if(opcao.equalsIgnoreCase("baixar&processar")){
-    		//listaOcorrenciasPoliciaisExtractor.parseListaEdownloadBOs(config.getPastaPaginasListagem(), config.getPastaLeituraBOs(), config.getSessionId());
-    		//boletimOcorrenciasExtractor.parseDocument(config.getPastaLeituraBOs(), config.getPastaGravacaoBOsProcessados());
-    	}
-    	System.out.println("Fim...");
-    }
+	@Autowired
+	private BoletimOcorrenciasExtractor boletimOcorrenciasExtractor;
 
-    public static void main(String[] args) {
-    	//opcao = args[0];
-    	opcao = "processar";
-        SpringApplication.run(Main.class, args);
-    }
+	@PostConstruct
+	public void iniciarDownload() throws IOException, InterruptedException {
+		if (opcao.equalsIgnoreCase("baixar")) {
+			listaOcorrenciasPoliciaisExtractor.parseListaEdownloadBOs(config.getPastaPaginasListagem(), config.getPastaLeituraBOs(), config.getSessionId());
+		}
+
+		else if (opcao.equalsIgnoreCase("processar")) {
+			boletimOcorrenciasExtractor.parseDocument(config.getPastaLeituraBOs(), config.getPastaGravacaoBOsProcessados());
+		}
+
+		else if (opcao.equalsIgnoreCase("baixar&processar")) {
+			listaOcorrenciasPoliciaisExtractor.parseListaEdownloadBOs(config.getPastaPaginasListagem(), config.getPastaLeituraBOs(), config.getSessionId());
+			boletimOcorrenciasExtractor.parseDocument(config.getPastaLeituraBOs(), config.getPastaGravacaoBOsProcessados());
+		}
+		System.out.println("Fim...");
+	}
+
+	public static void main(String[] args) {
+		opcao = args[0];
+		SpringApplication.run(Main.class, args);
+	}
 
 }
